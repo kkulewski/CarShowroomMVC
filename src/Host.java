9a29af69a42d13fb4942ac62f365a9401fd71e58
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-18 19:42:51 +0100

    Host app draft

--- src/Host.java
+++ src/Host.java
@@ -1,5 +1,22 @@
+import View.MainView;
+import Controller.MainController;
+import Model.Database;
+import Controller.ClientController;
 
-public class Host
-{
+public class Host {
+	
+	public static void main(String[] args)
+	{
+		Database database = new Database();
+		database.CreateTables();
+		
+		MainView mainView = new MainView();
+		mainView.setVisible(true);
+		
+		MainController mainController = new MainController(mainView);
+		
+		ClientController clientController = new ClientController();
+		clientController.InsertClient("Jan", "Kowalski", 12345678901L, "Warszawa", "Dluga 1");
+	}
 
 }
