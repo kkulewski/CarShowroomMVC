05df11bdb67b36e31de8e71d64e693b83406bbda
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-19 00:10:07 +0100

    Add dependencies to MainController

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,6 +1,9 @@
 package Controller;
 
 import java.awt.event.ActionListener;
+import Model.ClientModel;
+import Model.ClientTableModel;
+
 import java.awt.event.ActionEvent;
 
 import View.ClientAddView;
@@ -16,14 +19,17 @@ public class MainController
 	private ClientAddView clientAddView;
 	private ClientListView clientListView;
 	private ClientSearchView clientSearchView;
+	
+	private ClientModel clientModel;
 
 	
 	public MainController(MainView mainView)
 	{
 		this.mainView = mainView;
 		
+		this.clientModel = new ClientModel();
 		this.clientAddView = new ClientAddView();
-		this.clientListView = new ClientListView();
+		this.clientListView = new ClientListView(new ClientTableModel(clientModel.listClient()));
 		this.clientSearchView = new ClientSearchView();
 		
 		this.clientController = new ClientController(clientAddView);
