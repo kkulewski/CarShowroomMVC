3e7205e3f7a84db2040d061f749e709970c113a3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-18 21:14:52 +0100

    Bind Client partial views to buttons in MainView

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -3,15 +3,24 @@ package Controller;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import View.ClientAddView;
+import View.ClientListView;
+import View.ClientSearchView;
 import View.MainView;
 
 public class MainController 
 {
 	private MainView mainView;
+	private ClientAddView clientAddView;
+	private ClientListView clientListView;
+	private ClientSearchView clientSearchView;
 	
 	public MainController(MainView mainView)
 	{
 		this.mainView = mainView;
+		this.clientAddView = new ClientAddView();
+		this.clientListView = new ClientListView();
+		this.clientSearchView = new ClientSearchView();
 		
 		mainView.ClientAddListener(new ClientAddListener());
 		mainView.ClientListListener(new ClientListListener());
@@ -24,7 +33,10 @@ public class MainController
 		@Override
 		public void actionPerformed(ActionEvent arg0) 
 		{	
-			mainView.ActionCommited();	
+			//mainView.ActionCommited();	
+			mainView.setContentPane(clientAddView);
+			mainView.invalidate();
+			mainView.validate();
 		}
 	}
 	
@@ -33,7 +45,10 @@ public class MainController
 		@Override
 		public void actionPerformed(ActionEvent arg0) 
 		{	
-			mainView.ActionCommited();	
+			//mainView.ActionCommited();	
+			mainView.setContentPane(clientListView);
+			mainView.invalidate();
+			mainView.validate();
 		}
 	}
 	
@@ -42,7 +57,10 @@ public class MainController
 		@Override
 		public void actionPerformed(ActionEvent arg0) 
 		{	
-			mainView.ActionCommited();	
+			//mainView.ActionCommited();	
+			mainView.setContentPane(clientSearchView);
+			mainView.invalidate();
+			mainView.validate();
 		}
 	}
 	
