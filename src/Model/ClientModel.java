9f6ad5a85253fd17fa4d25fb92c80a49ae3f3ec4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-19 00:09:30 +0100

    Add listClient() mock

--- src/Model/ClientModel.java
+++ src/Model/ClientModel.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ClientModel extends Database 
 {
@@ -29,4 +30,20 @@ public class ClientModel extends Database
 		}
 		return true;
 	}
+	
+	
+	//STUB - GET DATA FROM DB, PUT INTO ARRAYLIST AND RETURN
+	public ArrayList<Client> listClient()
+	{
+		Client client1 = new Client(7, "Jan", "Kowalski", 99999999999L, "Warszawa", "Dluga 2");
+		Client client2 = new Client(7, "Tomasz", "Nowak", 12345678901L, "Gdansk", "Krotka 1");
+		Client client3 = new Client(7, "Mateusz", "Wisniewski", 55555555555L, "Poznan", "Srednia 3");
+		ArrayList<Client> clientList = new ArrayList<Client>();
+		clientList.add(client1);
+		clientList.add(client2);
+		clientList.add(client3);
+		
+		return clientList;
+	}
+	
 }
