ff9036659d6db6de8da3c60309ae6d5362abab15
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-19 00:09:45 +0100

    Add ClientListView implementation

--- src/View/ClientListView.java
+++ src/View/ClientListView.java
@@ -1,16 +1,26 @@
 package View;
 
 import java.awt.Color;
+import java.awt.GridBagLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JTable;
+
+import Model.ClientTableModel;
 
 public class ClientListView extends JPanel
 {
+	public JTable clientList;
 	
-	public ClientListView()
+	public ClientListView(ClientTableModel clientTableModel)
 	{
 		this.setVisible(true);
 		Color backgroundColor = new Color(100, 200, 0);
 		this.setBackground(backgroundColor);
+		this.setLayout(new GridBagLayout());
+		
+		this.clientList = new JTable(clientTableModel);
+		this.add(clientList);
+		
 	}
 }
