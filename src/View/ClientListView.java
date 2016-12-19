068a41b10584f43620feee9c0fa3db1deb4ef7d0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-19 21:38:43 +0100

    Add Edit and Remove buttons to ClientListView

--- src/View/ClientListView.java
+++ src/View/ClientListView.java
@@ -1,21 +1,22 @@
 package View;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
 
-import javax.swing.JMenuItem;
+import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 
 import Model.ClientTableModel;
 
 public class ClientListView extends JPanel
 {
 	public JTable clientList;
-	public JPopupMenu popupMenu;
-	public JMenuItem clientEdit;
-	public JMenuItem clientRemove;
+	public JButton editButton = new JButton("Edit");
+	public JButton removeButton = new JButton("Remove");
 	
 	public ClientListView(ClientTableModel clientTableModel)
 	{
@@ -23,19 +24,36 @@ public class ClientListView extends JPanel
 		Color backgroundColor = new Color(100, 200, 0);
 		this.setBackground(backgroundColor);
 		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		//EDIT BUTTON
+		//c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 0;
+		this.add(editButton, c);
+		//REMOVE BUTTON
+		//c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 0;
+		this.add(removeButton, c);
+		
+		//TABLE
+		//c.fill = GridBagConstraints.CENTER;
+		c.weightx = 1;
+		c.gridx = 1;
+		c.gridy = 1;
 		
 		this.clientList = new JTable(clientTableModel);
-		this.add(clientList);
+		this.clientList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.add(clientList, c);
 		
-		
-		//POPUP MENU
-		this.popupMenu = new JPopupMenu();
-		
-		this.clientEdit = new JMenuItem("Edit");
-		popupMenu.add(clientEdit);
-		this.clientRemove = new JMenuItem("Remove");
-		popupMenu.add(clientRemove);
-        setComponentPopupMenu(popupMenu);
+		/*
+		// GET SELECTED CLIENT
+		Client cl = clientTableModel.getClient(clientList.getSelectedRow());
+		System.out.println(cl.getId());
+		*/
 		
 	}
 }
