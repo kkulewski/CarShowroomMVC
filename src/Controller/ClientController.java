1a45a7dcfc9e99ad2f3118b5e99cd2f29b602682
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-20 21:42:15 +0100

    Ensure item being modified is selected in ClientEditListener

--- src/Controller/ClientController.java
+++ src/Controller/ClientController.java
@@ -71,6 +71,10 @@ public class ClientController
 		public void actionPerformed(ActionEvent arg0)
 		{
 			int selectedRow = clientListView.clientList.getSelectedRow();
+			//RETURN IF NO ROW SELECTED
+			if(selectedRow == -1)
+				return;
+			
 			Client selectedClient = clientListView.clientTableModel.getClient(selectedRow);
 			
 			int clientId = selectedClient.getId();
