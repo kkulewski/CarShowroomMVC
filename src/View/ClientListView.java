d43a94e29c8286ce92368a3c5a2e48195b90137a
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-26 01:40:25 +0100

    Fix ClientListView layout

--- src/View/ClientListView.java
+++ src/View/ClientListView.java
@@ -1,8 +1,6 @@
 package View;
 
 import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
@@ -29,20 +27,30 @@ public class ClientListView extends JPanel
 		this.setVisible(true);
 		Color backgroundColor = new Color(100, 200, 0);
 		this.setBackground(backgroundColor);
-		this.setLayout(new GridLayout(0,1));
+		this.setLayout(new GridLayout(2,1));
 		
-		//EDIT BUTTON
-		this.add(editButton);
-		//REMOVE BUTTON
-		this.add(removeButton);
 		
-		//TABLE
+		//LIST		
+		JPanel listPanel = new JPanel();
+		listPanel.setLayout(new GridLayout(1,1));
+		
 		this.clientTableModel = clientTableModel;
 		this.clientList = new JTable(this.clientTableModel);
 		this.clientList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
+		
 		this.scrollableClientList = new JScrollPane(clientList);
-		this.add(scrollableClientList);
+		this.scrollableClientList.setBackground(backgroundColor);
+		
+		listPanel.add(scrollableClientList);
+		this.add(listPanel);
+		
+		
+		//BUTTONS
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(1,2));
+		buttonPanel.add(editButton);
+		buttonPanel.add(removeButton);
+		this.add(buttonPanel);
 		
 	}
 	
