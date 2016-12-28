f934f0ecd5e3b85b916e24b77236a54dd73591f9
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-28 22:19:46 +0100

    Add PurchaseAddView class

---
+++ src/View/PurchaseAddView.java
@@ -0,0 +1,139 @@
+package View;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import Model.Car;
+import Model.CarModel;
+import Model.Client;
+import Model.ClientModel;
+import Model.Position;
+import Model.PositionModel;
+import Model.Worker;
+import Model.WorkerModel;
+
+public class PurchaseAddView extends JPanel
+{
+	//CLIENT
+	public JComboBox<String> clientCombo;
+	JLabel clientLabel = new JLabel("Client:");
+	//WORKER
+	public JComboBox<String> workerCombo;
+	JLabel workerLabel = new JLabel("Worker:");
+	//CAR
+	public JComboBox<String> carCombo;
+	JLabel carLabel = new JLabel("Car:");
+	//DATE
+	public JTextField transaction_dateField = new JTextField(20);
+	JLabel transaction_dateLabel = new JLabel("Date:");
+	//SUBMIT BUTTON
+	public JButton submitButton = new JButton("Add");
+	
+	public PurchaseAddView()
+	{
+		this.setVisible(true);
+		Color backgroundColor = new Color(100, 200, 0);
+		this.setBackground(backgroundColor);
+		this.setLayout(new GridLayout(2,1));
+		
+		//DATA
+		JPanel dataPanel = new JPanel();
+		dataPanel.setLayout(new GridLayout(0,1));
+		
+		//CLIENT
+		dataPanel.add(clientLabel);
+		ClientModel clientModel = new ClientModel();
+		ArrayList<Client> clientList = new ArrayList<Client>();
+		clientList = clientModel.list();
+		
+	    clientCombo = new JComboBox<String>();
+	    for(Client c : clientList)
+	    {
+	    	clientCombo.addItem(c.getSurname());
+	    }
+	    clientCombo.setSelectedIndex(0);
+	    JScrollPane clientScrollPane = new JScrollPane(clientCombo);    
+		dataPanel.add(clientScrollPane);
+		
+		//WORKER
+		dataPanel.add(workerLabel);
+		WorkerModel workerModel = new WorkerModel();
+		ArrayList<Worker> workerList = new ArrayList<Worker>();
+		workerList = workerModel.list();
+				
+	    workerCombo = new JComboBox<String>();
+	    for(Worker w : workerList)
+	    {
+	    	workerCombo.addItem(w.getSurname());
+	    }
+	    workerCombo.setSelectedIndex(0);
+	    JScrollPane workerScrollPane = new JScrollPane(workerCombo);    
+		dataPanel.add(workerScrollPane);
+				
+		//POSITION
+		dataPanel.add(carLabel);
+		CarModel carModel = new CarModel();
+		ArrayList<Car> carList = new ArrayList<Car>();
+		carList = carModel.list();
+		
+	    carCombo = new JComboBox<String>();
+	    for(Car c : carList)
+	    {
+	    	carCombo.addItem(c.getModel());
+	    }
+	    carCombo.setSelectedIndex(0);
+	    JScrollPane carScrollPane = new JScrollPane(carCombo);    
+		dataPanel.add(carScrollPane);
+		
+		this.add(dataPanel);
+		
+		
+		//BUTTONS
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(1,1));
+		//SUBMIT
+		buttonPanel.add(submitButton);
+		this.add(buttonPanel);
+	}
+	
+	public void DisplaySuccessPopup()
+	{
+		JPanel panel = new JPanel(new GridLayout(0, 1));
+	    
+		JLabel text = new JLabel("Purchase added succesfully.");
+		panel.add(text);
+		
+		JOptionPane.showConfirmDialog(null, panel, "Purchase added", JOptionPane.PLAIN_MESSAGE);
+	}
+	
+	public void DisplayErrorPopup(List<String> errorMessage)
+	{
+		JPanel panel = new JPanel(new GridLayout(0, 1));
+	    
+		JLabel text = new JLabel("Error: ");
+		panel.add(text);
+		for(String s : errorMessage)
+		{
+			panel.add(new JLabel(s));
+		}
+
+		
+		JOptionPane.showConfirmDialog(null, panel, "Error", JOptionPane.PLAIN_MESSAGE);	
+	}
+	
+	public void PurchaseAddSubmitListener(ActionListener purchaseAddSubmitListener) 
+	{
+		submitButton.addActionListener(purchaseAddSubmitListener);
+	}
+}
