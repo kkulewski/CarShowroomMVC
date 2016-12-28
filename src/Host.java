90eb53cce95403c5f2f874adec64d7d297f499d2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2016-12-29 00:00:57 +0100

    Move MainController into try-catch

--- src/Host.java
+++ src/Host.java
@@ -6,16 +6,21 @@ public class Host {
 	
 	public static void main(String[] args)
 	{
-		Database database = new Database();
-		database.CreateTables();
-		
-		MainView mainView = new MainView();
-		mainView.setVisible(true);
-		
-		MainController mainController = new MainController(mainView);
-		mainController.Run();
-		
-		//mainController.FillWithTestData();
+		try
+		{
+			Database database = new Database();
+			database.CreateTables();
+			
+			MainView mainView = new MainView();
+			mainView.setVisible(true);
+			
+			MainController mainController = new MainController(mainView);
+			mainController.Run();
+			
+			//TEST DATA
+			//mainController.FillWithTestData();
+		}
+		catch(Exception e){ }
 	}
 
 }
